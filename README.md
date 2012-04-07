dfb48535ed40b4a1453a783b38e518e84b381c13
Johannes J. Schmidt <schmidt@netzmerk.com>
2012-04-07 23:18:29 +0200

    Initial commit with working Todo example

---
+++ README.md
@@ -0,0 +1,10 @@
+Super simple version with working Todo application.
+
+# backbone-pouchdb
+
+A Backbone sync adapter for PouchDB (https://raw.github.com/daleharvey/pouchdb).
+
+### Getting started
+
+Check out the Todo example how it works.
+Basically, just set a `database` property on your model or collection.
---
+++ backbone-couchdb.js
@@ -0,0 +1,70 @@
+// CouchDB specific adjustments to Backbone
+
+// id attribute
+// http://backbonejs.org/docs/backbone.html#section-34
+Backbone.Model.prototype.idAttribute = '_id';
+
+
+// parse models
+Backbone.Model.prototype.parse = function(response) {
+  // adjust rev
+  if (response.rev) {
+    response._rev = response.rev;
+    delete response.rev;
+  }
+
+  // adjust id
+  if (response.id) {
+    response._id = response.id;
+    delete response.id;
+  }
+
+  // remove ok
+  delete response.ok;
+
+  return response;
+};
+
+
+// parse collections
+Backbone.Collection.prototype.parse = function(response) {
+  return response.rows && _.map(response.rows, function(row) { return row.doc });
+};
+
+
+// save models (update rev)
+Backbone.Model.prototype.save = (function() {
+  var oldSave = Backbone.Model.prototype.save;
+
+  return function(attributes, options) {
+    options || (options = {});
+
+    var success = options.success;
+
+    options.success = function(model, response) {
+      model.set({ _rev: response._rev }, { silent: true });
+
+      if (typeof success === 'function') {
+        success(model, response);
+      }
+    };
+
+    oldSave.call(this, attributes, options);
+  };
+})();
+
+
+// delete models (append rev)
+Backbone.Model.prototype.destroy = (function() {
+  var oldDestroy = Backbone.Model.prototype.destroy;
+
+  return function(options) {
+    options || (options = {});
+    options.headers || (options.headers = {});
+
+    options.headers['If-Match'] = this.get('_rev');
+
+    oldDestroy.call(this, options);
+  };
+})();
+
