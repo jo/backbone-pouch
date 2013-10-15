ccbb9a6c22d38b91738b70915c61a8c9410698a4
Johannes J. Schmidt <schmidt@netzmerk.com>
2013-10-15 22:20:24 +0200

    compiled release artefacts

--- dist/backbone-pouch.js
+++ dist/backbone-pouch.js
@@ -1,4 +1,4 @@
-/*! backbone-pouch - v1.1.0 - 2013-10-15
+/*! backbone-pouch - v1.1.1 - 2013-10-15
 * http://jo.github.io/backbone-pouch/
 * Copyright (c) 2013 Johannes J. Schmidt; Licensed MIT */
 (function(root) {
